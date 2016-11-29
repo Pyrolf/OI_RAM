5a72ac1a656a2cf83faadb50b5fa9258927b3f09
Pyrolf <pyroflame11@gmail.com>
2016-11-29 11:07:53 +0800

    change design resolution

--- Classes/AppDelegate.cpp
+++ Classes/AppDelegate.cpp
@@ -8,7 +8,7 @@
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(1280, 720) * 0.5f;
+static cocos2d::Size designResolutionSize = cocos2d::Size(1280, 720) * 0.7f;
 static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
 static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
 static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
@@ -47,7 +47,7 @@ bool AppDelegate::applicationDidFinishLaunching() {
     auto glview = director->getOpenGLView();
     if(!glview) {
 #if (CC_TARGET_PLATFORM == CC_PLATFORM_WIN32) || (CC_TARGET_PLATFORM == CC_PLATFORM_MAC) || (CC_TARGET_PLATFORM == CC_PLATFORM_LINUX)
-		glview = GLViewImpl::createWithRect("OI_RAM", cocos2d::Rect(0, 0, largeResolutionSize.width, largeResolutionSize.height));
+		glview = GLViewImpl::createWithRect("OI_RAM", cocos2d::Rect(0, 0, designResolutionSize.width, designResolutionSize.height));
 #else
         glview = GLViewImpl::create("OI_RAM");
 #endif
